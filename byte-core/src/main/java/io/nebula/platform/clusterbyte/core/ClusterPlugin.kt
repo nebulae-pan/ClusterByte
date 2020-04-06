4822c82506c8f11546ecfb79ac960ba1bf020bed
nebulae-pan <nebula0929@gmail.com>
2020-04-06 15:43:26 +0800

    init commit

---
+++ byte-core/src/main/java/io/nebula/platform/clusterbyte/core/ClusterPlugin.kt
@@ -0,0 +1,13 @@
+package io.nebula.platform.clusterbyte.core
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by nebula on 2020-04-05
+ */
+class ClusterPlugin : Plugin<Project> {
+    override fun apply(p0: Project) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
