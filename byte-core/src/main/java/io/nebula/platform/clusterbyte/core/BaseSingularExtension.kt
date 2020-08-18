b05035d51e628d7d2ae584a45bc59e1f1ed22958
nebulae-pan <nebula0929@gmail.com>
2020-08-18 21:23:10 +0800

    extension auto instant support

--- byte-core/src/main/java/io/nebula/platform/clusterbyte/core/BaseSingularExtension.kt
+++ byte-core/src/main/java/io/nebula/platform/clusterbyte/core/BaseSingularExtension.kt
@@ -6,4 +6,5 @@ package io.nebula.platform.clusterbyte.core
  * date : 2020-07-15 14:50
  */
 abstract class BaseSingularExtension {
+    abstract var closureName: String
 }
--- byte-core/src/main/java/io/nebula/platform/clusterbyte/core/BaseSingularPlugin.kt
+++ byte-core/src/main/java/io/nebula/platform/clusterbyte/core/BaseSingularPlugin.kt
@@ -1,24 +1,31 @@
 package io.nebula.platform.clusterbyte.core
 
 import com.android.build.gradle.BaseExtension
+import com.google.common.reflect.TypeToken
 import io.nebula.platform.clusterbyte.converter.ConverterFactory
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.model.ObjectFactory
+import sun.reflect.generics.reflectiveObjects.NotImplementedException
+import javax.inject.Inject
 
 /**
  * @author xinghai.pan
  *
  * date : 2020-07-14 16:50
  */
-abstract class BaseSingularPlugin : Plugin<Project> {
+abstract class BaseSingularPlugin<E : BaseSingularExtension> : Plugin<Project> {
     protected var androidExtension: BaseExtension? = null
     protected var clusterExtension: ClusterExtension? = null
+    protected lateinit var extension: E
+
     private lateinit var project: Project
 
     override fun apply(p: Project) {
         androidExtension = p.extensions.findByType(BaseExtension::class.java)
         clusterExtension = p.extensions.findByType(ClusterExtension::class.java)
         project = p
+        extension = objectFactory().newInstance(reflectForExtensionType())
         apply0(p)
     }
 
@@ -53,5 +60,14 @@ abstract class BaseSingularPlugin : Plugin<Project> {
             return str.substring(1, str.length)
         }
         return str
+    }
+
+    @Inject
+    fun objectFactory(): ObjectFactory {
+        throw NotImplementedException()
+    }
+
+    private fun reflectForExtensionType(): Class<E> {
+        return object : TypeToken<E>(javaClass) {}.rawType as Class<E>
     }
 }
